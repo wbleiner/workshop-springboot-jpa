a4f7f2b4cf67612dec575d5e3e89d8534667f36b
Wanderson Bleiner <wanderson.bleiner@gmail.com>
2026-09-06 19:44:12 -0300

    Sevice Layer, componet registration

--- src/main/java/com/wandersonbleiner/course/resources/UserResource.java
+++ src/main/java/com/wandersonbleiner/course/resources/UserResource.java
@@ -1,20 +1,36 @@
 package com.wandersonbleiner.course.resources;
 
 import com.wandersonbleiner.course.entities.User;
+import com.wandersonbleiner.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll()
+    public ResponseEntity<List<User>> findAll()
     {
-        User user = new User(1L, "Maria", "maria@email.com", "999999999", "1234");
-        return ResponseEntity.ok().body(user);
+        var users = service.findAll();
+        return ResponseEntity.ok().body(users);
     }
 
+    @GetMapping(value = "/{id}")
+    public  ResponseEntity<User> findById(@PathVariable Long id)
+    {
+        var user = service.findById(id);
+
+        return ResponseEntity.ok().body(user);
+    }
 }
