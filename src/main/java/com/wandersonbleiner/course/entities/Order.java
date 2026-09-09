677814b0bc3cd226924826bfdb4a25d6c40982df
Wanderson Bleiner <wanderson.bleiner@gmail.com>
2026-09-09 15:56:34 -0300

    Created OrderStatus enum

--- src/main/java/com/wandersonbleiner/course/entities/Order.java
+++ src/main/java/com/wandersonbleiner/course/entities/Order.java
@@ -1,6 +1,7 @@
 package com.wandersonbleiner.course.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.wandersonbleiner.course.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -11,63 +12,90 @@ import java.util.Objects;
 
 @Entity
 @Table(name = "tb_order")
-public class Order implements Serializable {
+public class Order implements Serializable
+{
     @Serial
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
-    private Instant moment;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+    private Instant moment;
+    private Integer orderStatus;
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
-    public Order(Long id, Instant moment, User client) {
+
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client)
+    {
         this.id = id;
         this.moment = moment;
         this.client = client;
+        setOrderStatus(orderStatus);
     }
 
-    public Order() {
+    public Order()
+    {
     }
 
-    public Long getId() {
+    public Long getId()
+    {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(Long id)
+    {
         this.id = id;
     }
 
-    public Instant getMoment() {
+    public Instant getMoment()
+    {
         return moment;
     }
 
-    public void setMoment(Instant moment) {
+    public void setMoment(Instant moment)
+    {
         this.moment = moment;
     }
 
-    public User getClient() {
+    public OrderStatus getOrderStatus()
+    {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus)
+    {
+        if (orderStatus != null)
+        {
+
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
+
+    public User getClient()
+    {
         return client;
     }
 
-    public void setClient(User client) {
+    public void setClient(User client)
+    {
         this.client = client;
     }
 
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(Object o)
+    {
         if (o == null || getClass() != o.getClass()) return false;
         Order order = (Order) o;
         return Objects.equals(id, order.id);
     }
 
     @Override
-    public int hashCode() {
+    public int hashCode()
+    {
         return Objects.hashCode(id);
     }
 }
